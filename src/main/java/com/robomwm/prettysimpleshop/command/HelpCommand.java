d859fbfed79c7782bfad88dee93c016725c495c4
RoboMWM <robomwm@users.noreply.github.com>
2018-03-27 19:48:37 -0700

    /shop command message is configurable

--- src/main/java/com/robomwm/prettysimpleshop/command/HelpCommand.java
+++ src/main/java/com/robomwm/prettysimpleshop/command/HelpCommand.java
@@ -1,6 +1,5 @@
 package com.robomwm.prettysimpleshop.command;
 
-import com.robomwm.prettysimpleshop.ConfigManager;
 import com.robomwm.prettysimpleshop.PrettySimpleShop;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -14,16 +13,15 @@ import org.bukkit.command.CommandSender;
  */
 public class HelpCommand implements CommandExecutor
 {
-    private ConfigManager configManager;
+    private PrettySimpleShop plugin;
     public HelpCommand(PrettySimpleShop plugin)
     {
-        configManager = plugin.getConfigManager();
+        this.plugin = plugin;
     }
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
-        sender.sendMessage("To create a shop, place a chest named " +
-                ChatColor.ITALIC + configManager.getString("shopName") + ChatColor.RESET +
-                " and use /setprice.\nUse /buy to buy from a shop.");
+        sender.sendMessage(ChatColor.LIGHT_PURPLE + "PrettySimpleShop version " + plugin.getDescription().getVersion() + " by " + ChatColor.RED + "RoboMWM");
+        sender.sendMessage("shopCommand");
         return true;
     }
 }
