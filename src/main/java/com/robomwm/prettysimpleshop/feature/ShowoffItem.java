6ae9c8f5ec010ab17bed24794ecd0d7e16f0a2ca
RoboMWM <robomwm@users.noreply.github.com>
2018-04-16 02:07:14 -0700

    don't attempt to spawn null items
    Also don't attempt to use github's stupid web editor/web anything

--- src/main/java/com/robomwm/prettysimpleshop/feature/ShowoffItem.java
+++ src/main/java/com/robomwm/prettysimpleshop/feature/ShowoffItem.java
@@ -129,9 +129,11 @@ public class ShowoffItem implements Listener
                             Chest chest = shopAPI.getChest(location);
                             if (chest == null || !shopAPI.isShop(chest, false))
                                 continue;
-                            if (spawnItem(new ShopInfo(shopAPI.getLocation(chest), plugin.getShopAPI().getItemStack(chest), plugin.getShopAPI().getPrice(chest)))
-                                    && noShops)
-                                noShops = false;
+                            ItemStack item = shopAPI.getItemStack(chest);
+                            if (item == null)
+                                continue;
+                            if (spawnItem(new ShopInfo(shopAPI.getLocation(chest), item, plugin.getShopAPI().getPrice(chest)))
+                                noShops = false; //Shops exist in this chunk
                         }
                         if (noShops)
                             removeCachedChunk(chunk);
